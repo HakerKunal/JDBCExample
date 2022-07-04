facfd88025b3288d8d348d2dfb15c20dac88e385
KunalHaker <kunal.bathamn@quovantis.com>
2022-07-04 12:04:09 +0530

    Added JDBC Example

---
+++ JdbcTest/src/main/java/com/example/jdbctest/com/example/jdbctest/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.example.jdbctest.com.example.jdbctest.repository;
+
+import com.example.jdbctest.model.Tutorial;
+
+import java.util.List;
+
+public interface TutorialRepository {
+    int save(Tutorial book);
+    int update(Tutorial book);
+    Tutorial findById(Long id);
+    int deleteById(Long id);
+    List<Tutorial> findAll();
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+    int deleteAll();
+}
